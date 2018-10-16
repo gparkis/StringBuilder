1531f565b3eaed896d4f89848955513da785530d
Grant Parkis <gparkis@gmail.com>
2018-10-16 19:58:38 -0400

    starting code

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName = "Grant ";
+            string lastName = "Parkis ";
+            string expression = "kills it with Angular Apps";\
+            string concatenation = firstName + lastName + expression;
+            Console.WriteLine(concatenation);
+
+            Console.WriteLine(concatenation.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+
+            Console.Read();
+        }
+    }
+
+    
+}
