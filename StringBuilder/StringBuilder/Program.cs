8e4fb20be7d592aaaa37dca05ea432c208385d14
Grant Parkis <gparkis@gmail.com>
2018-10-16 20:04:20 -0400

    final code

--- StringBuilder/StringBuilder/Program.cs
+++ StringBuilder/StringBuilder/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
 using System.Text;
 
-{
+
     class Program
     {
         static void Main()
         {
             string firstName = "Grant ";
             string lastName = "Parkis ";
-            string expression = "kills it with Angular Apps";\
+            string expression = "kills it with Angular Apps";
             string concatenation = firstName + lastName + expression;
             Console.WriteLine(concatenation);
 
             Console.WriteLine(concatenation.ToUpper());
 
             StringBuilder sb = new StringBuilder();
+        sb.Append("There once was a fat man");
+        sb.Append("\nAnd he wore a red and white suit, a white that matched his beard");
+        sb.Append("\nThis fat man also road a sleigh that flew through the sky");
+        Console.WriteLine(sb);
 
             Console.Read();
         }
     }
 
     
-}
+
